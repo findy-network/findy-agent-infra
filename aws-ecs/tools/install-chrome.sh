7957a6559befcd00e0b9ac6b422956da3ce9cfc5
Laura Vuorenoja <laura.vuorenoja@gmail.com>
2023-03-24 15:02:17 +0200

    Fix bash script formatting.

--- aws-ecs/tools/install-chrome.sh
+++ aws-ecs/tools/install-chrome.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-RUN set -ex &&
-  wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add - &&
-  echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | tee /etc/apt/sources.list.d/google.list &&
-  apt-get update &&
-  apt-get install -y -qq google-chrome-stable &&
-  google-chrome --version
+set -ex
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | tee /etc/apt/sources.list.d/google.list
+apt-get update
+apt-get install -y -qq google-chrome-stable
+google-chrome --version
