9c397c18506b05d6e076cbb81577a503e8e2beb3
Laura Vuorenoja <laura.vuorenoja@gmail.com>
2023-05-02 16:56:42 +0300

    Use e2e tests from common action.

--- aws-ecs/tools/create-set-env.sh
+++ aws-ecs/tools/create-set-env.sh
@@ -22,8 +22,3 @@ awk "$sub_cmd" "./build/set-env.sh" >"./set-env.sh.tmp" && mv ./set-env.sh.tmp .
 # replace API server port
 sub_cmd='{sub("50052","'$API_PORT'")}1'
 awk "$sub_cmd" "./build/set-env.sh" >"./set-env.sh.tmp" && mv ./set-env.sh.tmp ./build/set-env.sh
-
-# insert cert dl command
-dl_cert_cmd='  echo -n | openssl s_client -connect \"'$API_URL:$API_PORT'\" -servername \"'$API_URL:$API_PORT'\" | openssl x509 >./cert/server/server.crt'
-awk_cmd='/^  echo "# agency API server TLS cert path"/ && !modif { printf("  rm ./cert/server/server.crt\n'$dl_cert_cmd'\n"); modif=1 } {print}'
-awk "$awk_cmd" "./build/set-env.sh" >"./set-env.sh.tmp" && mv ./set-env.sh.tmp ./build/set-env.sh
