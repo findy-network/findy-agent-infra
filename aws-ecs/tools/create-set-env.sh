6df9691eab47d9a27953ea9147c79d3135f5dbf4
Laura Vuorenoja <laura.vuorenoja@gmail.com>
2023-05-09 13:14:30 +0300

    Add set-env-cli script to deployment.

--- aws-ecs/tools/create-set-env.sh
+++ aws-ecs/tools/create-set-env.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
 TEMPLATE_PATH=$1
-APP_URL=$2
-API_URL=$3
-API_PORT=$4
+TARGET_PATH=$2
+APP_URL=$3
+API_URL=$4
+API_PORT=$5
 
-cp $TEMPLATE_PATH ./build/set-env.sh
+cp $TEMPLATE_PATH $TARGET_PATH
 
 # replace auth server URL
 sub_cmd='{gsub("http://localhost:8088","https://'$APP_URL'")}1'
-awk "$sub_cmd" "./build/set-env.sh" >"./set-env.sh.tmp" && mv ./set-env.sh.tmp ./build/set-env.sh
+awk "$sub_cmd" "$TARGET_PATH" >"./set-env.sh.tmp" && mv ./set-env.sh.tmp $TARGET_PATH
 
 # replace auth origin URL
 sub_cmd='{gsub("http://localhost:3000","https://'$APP_URL'")}1'
-awk "$sub_cmd" "./build/set-env.sh" >"./set-env.sh.tmp" && mv ./set-env.sh.tmp ./build/set-env.sh
+awk "$sub_cmd" "$TARGET_PATH" >"./set-env.sh.tmp" && mv ./set-env.sh.tmp $TARGET_PATH
 
 # replace API server domain
 sub_cmd='{gsub("localhost","'$API_URL'")}1'
-awk "$sub_cmd" "./build/set-env.sh" >"./set-env.sh.tmp" && mv ./set-env.sh.tmp ./build/set-env.sh
+awk "$sub_cmd" "$TARGET_PATH" >"./set-env.sh.tmp" && mv ./set-env.sh.tmp $TARGET_PATH
 
 # replace API server port
 sub_cmd='{gsub("50052","'$API_PORT'")}1'
-awk "$sub_cmd" "./build/set-env.sh" >"./set-env.sh.tmp" && mv ./set-env.sh.tmp ./build/set-env.sh
+awk "$sub_cmd" "$TARGET_PATH" >"./set-env.sh.tmp" && mv ./set-env.sh.tmp $TARGET_PATH
